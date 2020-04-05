b70339283bbe17094f01ae46f5b0fb0a0d3302a4
Eduardo Núñez León <leoneduardon@gmail.com>
2020-04-05 15:08:52 -0600

    first commit

--- sketch.js
+++ sketch.js
@@ -1,54 +1,59 @@
-let ball;
-let actualBall;
-let ballWidth = 40;
-let ballHeight = 40;
-let ballNumber=0;
-var balls=[];
-let dx;
-let dy;
-let distance;
+let ww = window.screen.width;
+let wh = window.screen.height-113;
+
+const p1= {x:0, y:0};
+const p2= {x:ww, y:wh};
+	
+const p3= {x:0, y:wh};
+const p4= {x:ww, y:0};
+ 
+const p5= {x:0, y:wh/2};
+const p6= {x:ww, y:wh/2};
+
+const p7= {x:ww/2, y:0};
+const p8= {x:ww/2, y:wh};
+
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-		background('white');
-		balls.forEach(function(ball){
-	    	actualBall = ball;
-	    	actualBall.draw();
-	    	actualBall.move();
-	    	balls.forEach(function(ball){
-				otherBall = ball;
-				/*dx = actualBall.x - otherBall.x;
-				dy = actualBall.x - otherBall.x;
-				distance = Math.sqrt(dx * dx + dy * dy);
-				if (actualBall.getballNumber != otherBall.getballNumber) {
-					if (distance < actualBall.radius + otherBall.radius) {
-					    		actualBall.cambiarDireccion();
-		    					otherBall.cambiarDireccion();
-					}
-				}*/
-				if (actualBall.getballNumber != otherBall.getballNumber) {
-					if ((actualBall.posX >= otherBall.posX && actualBall.posX <= otherBall.posX + (actualBall.width)) && 
-					   (actualBall.posY >= otherBall.posY && actualBall.posY <= otherBall.posY + (actualBall.height))) {
-	    					
-	    					actualBall.cambiarDireccion();
-	    					otherBall.cambiarDireccion();
-	    			}
-				}
-			});
-		});
+	background('white');
+	ecuapp(p1,p2);
+	ecuapp(p3,p4);
+	ecuapp(p5,p6);
+	cruceVertical(p7,p8);
+	console.log(wh/2);
 }
 
+function ecuapp(p1,p2) {
+	const dx = p2.x - p1.x;
+ 	const dy = p2.y - p1.y;
 
-function mousePressed() {
-	ball = new Ball(mouseX,mouseY,getRandomColor(),50,floor(windowWidth),floor(windowHeight),ballWidth,ballHeight,ballNumber);
-	balls.push(ball);
-	ballNumber+=1;
+	const m = dy / dx;
+	const b = p1.y - m * p1.x;
+
+	point(p1.x, p1.y);
+
+	let x = p1.x + 1;
+	let y;
+	while(x != p2.x) {
+		y = m * x + b;
+		point(x, y);
+		x++;
+	}
 }
 
-function getRandomColor() {
-  randomColor = color(random(255),random(255),random(255));
-  return randomColor;
+function cruceVertical(p1,p2) {
+	const dx=p1.x;
+	const dy=p1.y;
+
+	let y= dy;
+
+	while(y != p2.y){
+		point(dx,y);
+		y++;
+	}
+
 }
